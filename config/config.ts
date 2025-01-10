28e8f6b8a8c7ae208f2a996ce38646b18ba69d1e
ssdeanx <ssdeanx@gmail.com>
2025-01-10 15:27:51 -0500

    Refactor Cosmos DB configuration to use environment variables; implement item retrieval service and utility functions for type checking and className management

--- config/config.ts
+++ config/config.ts
@@ -1,8 +1,8 @@
 // config.ts
 export default {
   cosmosDB: {
-    endpoint: "{URI}",
-    key: "{Cosmos-Key}",
+    endpoint: process.env.COSMOS_DB_ENDPOINT || "",
+    key: process.env.COSMOS_DB_KEY || "",
     databaseId: "sample-database",
     containerId: "sample-container",
   },
